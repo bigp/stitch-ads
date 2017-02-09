c12ec3e86a168a665d50507950c375359fe08c33
Pierre Chamberlain <bigp@pierrechamberlain.ca>
2017-02-09 01:44:16 -0400

    Cleaned up messy comments.

--- index.js
+++ index.js
@@ -1,63 +1,7 @@
-"use strict";
-
-/**
- * Created by Chamberlain on 23/08/2016.
- */
-
 const mkdirp = require('mkdirp');
 const stitchCls = require("./src/libs/stitch");
 const stitch = stitchCls.create();
 traceClear();
-
-/*
-var DO_IT = false;
-(function() {
-    if(!DO_IT) return false;
-    const path = require('path');
-    var data = fileRead(path.resolve(__stitch, './assets/datauri.demo.txt'));
-
-    require('./src/libs/string_utils');
-    
-    function timeThis(cb) {
-        var timeBefore = new Date().getTime();
-        cb();
-        var timeNow = new Date().getTime();
-        trace("It took {0}ms.".format(timeNow-timeBefore));
-    }
-
-    timeThis(() => {
-        trace(findMostCommonSubstr(data, {
-            minCount: 10,
-            minLength: 2,
-            maxLength: 7
-        }));
-    });
-
-    timeThis(() => {
-        trace(findMostCommonSubstr("aaabbbcccdddaaaddadddbbdbbdbbbdbbdbbb", {
-            minCount: 10,
-            minLength: 2,
-            maxLength: 7
-        }));
-    });
-
-    timeThis(() => {
-        trace(findMostCommonSubstr(data, {
-            minCount: 10,
-            minLength: 2,
-            maxLength: 7
-        }));
-    });
-    
-
-    
-    return true;
-})();
-if(DO_IT) return;
-*/
-
-
-//trace("Stitch Started in: " + __project);
 
 mkdirp(__project + "/public");
 mkdirp(__project + "/ads");
